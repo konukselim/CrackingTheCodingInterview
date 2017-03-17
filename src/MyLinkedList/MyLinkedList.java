9aafa2fd7a399cc1f804d37ec1ac6b393e7177f7
selimkonuk <selimkonuktr@gmail.com>
2017-03-18 02:07:36 +0300

    Chapter 2 First Question -- DONE --

---
+++ src/MyLinkedList/MyLinkedList.java
@@ -0,0 +1,130 @@
+package MyLinkedList; /**
+ * Created by selimkonuk on 17/03/2017.
+ */
+
+public class MyLinkedList<T>{
+
+    private int size;
+    public Node head;
+    public Node back;
+
+    public class Node{
+        public T item;
+        public Node next;
+        public Node prev;
+    }
+
+    public MyLinkedList(){
+        head = null;
+        back = null;
+        size = 0;
+    }
+
+    public void addLast(T item){
+        Node node = new Node();
+        if(head == null){
+            node.next = null;
+            node.prev = null;
+            node.item = item;
+
+            head = node;
+            back = node;
+        }
+        else{
+            node.next = null;
+            node.prev = back;
+            node.item = item;
+
+            back.next = node;
+            back = node;
+        }
+        size++;
+    }
+
+    public void addFirst(T item){
+        Node node = new Node();
+        if(head == null){
+            node.next = null;
+            node.prev = null;
+            node.item = item;
+
+            head = node;
+            back = node;
+        }
+        else{
+            node.next = head;
+            node.prev = null;
+            node.item = item;
+
+            head.prev = node;
+            head = node;
+        }
+        size++;
+    }
+
+    public void deleteItem(T item){
+        Node current = head;
+        while(current != null){
+            if(current.item == item){
+                if(current == head){
+                    head = current.next;
+                    head.prev = null;
+                }
+                else if(current == back){
+                    back = current.prev;
+                    back.next = null;
+                }
+                else{
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+            }
+            current = current.next;
+        }
+    }
+
+    public void deleteIndice(int indice){
+        if(indice <0 || indice >= size){
+            if(size == 0){
+                System.out.println("No item in the Linked List!!");
+            }
+            else{
+                System.out.println("Please provide an indice from 0 to " + (size-1) );
+            }
+            return;
+        }
+
+        Node current = head;
+        for(int i=0; i<indice ;i++){
+            current = current.next;
+        }
+
+        if(current == head){
+            head = current.next;
+            head.prev = null;
+        }
+        else if(current == back){
+            back = current.prev;
+            back.next = null;
+        }
+        else{
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+
+    }
+
+    public void printMyList(){
+        Node current = head;
+        while(current.next != null){
+            System.out.print(current.item + " " + "<-> ");
+            current = current.next;
+        }
+        System.out.print(current.item);
+    }
+
+    public int size(){
+        return size;
+    }
+
+}
---
+++ src/MyLinkedList/Q2_1/RemoveDuplicates.java
@@ -0,0 +1,79 @@
+package MyLinkedList.Q2_1; /**
+ * Created by selimkonuk on 17/03/2017.
+ */
+
+import MyLinkedList.MyLinkedList;
+import java.util.HashSet;
+
+
+// Suppose that this is a one way LinkedList
+
+public class RemoveDuplicates {
+
+    // O(N)
+    public static MyLinkedList withTemp(MyLinkedList l){
+
+        MyLinkedList.Node curr = l.head;
+        MyLinkedList.Node temp = null;
+        HashSet container = new HashSet();
+
+        while(curr != null){
+            if(container.contains(curr.item)){
+                temp.next = curr.next;
+            }
+            else{
+                container.add(curr.item);
+                temp = curr;
+            }
+            curr = curr.next;
+        }
+
+        return l;
+    }
+
+    // O(N^2)
+    public static MyLinkedList withoutTemp(MyLinkedList l){
+
+        MyLinkedList.Node curr = l.head;
+        while(curr != null){
+            MyLinkedList.Node iter = curr;
+            while(iter.next != null){
+                if(iter.next.item.equals(curr.item)){
+                    iter.next = iter.next.next;
+                }
+                else{
+                    iter = iter.next;
+                }
+            }
+            curr = curr.next;
+        }
+
+
+        return l;
+    }
+
+    public static void main(String args[]){
+        MyLinkedList l = new MyLinkedList();
+        l.addLast(1);
+        l.addLast(2);
+        l.addLast(3);
+        l.addLast(4);
+        l.addLast(5);
+        l.addLast(3);
+        l.addLast(2);
+        l.addLast(8);
+        l.addLast(3);
+        l.addLast(2);
+        l.addLast(9);
+        l.addLast(10);
+        l.addLast(9);
+        l.addLast(0);
+
+
+        //withTemp(l);
+        withoutTemp(l);
+        l.printMyList();
+
+    }
+
+}
